c37718a40c46a519658f412580509011e5a95ca2
Kaori Tsushima <kaoritsushima0107@gmail.com>
2023-09-03 11:28:26 +0100

    connected aws dynamoDB

--- src/index.js
+++ src/index.js
@@ -1,11 +1,24 @@
 import express from "express";
+import dotenv from "dotenv";
+import { DynamoDBClient, ListTablesCommand } from "@aws-sdk/client-dynamodb";
+
+dotenv.config();
+const dbClient = new DynamoDBClient({
+  region: process.env.REGION,
+  credentials: {
+    accessKeyId: process.env.ACCESS_KEY_ID,
+    secretAccessKey: process.env.SECRET_ACCESS_KEY,
+  },
+});
 
 const app = express();
 const port = 8000;
 
 app.use(express.json());
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.get("/", async (req, res) => {
+  const command = new ListTablesCommand({});
+  const response = await dbClient.send(command);
+  res.send(response);
 });
 
 app.post("/login", (req, res) => {
